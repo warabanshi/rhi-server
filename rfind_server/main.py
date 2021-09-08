a088885f1fb519216b0f4e0167ed1a5d3567455a
warabanshi <warabanshi@gmail.com>
2021-09-08 12:37:43 +0900

    Added actions

--- rfind_server/main.py
+++ rfind_server/main.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from typing import Any, Dict, List
 from werkzeug.wrappers import Request, Response
@@ -8,11 +9,28 @@ STORAGE_PATH = '/tmp/rfind-server.store'
 
 
 def store_command(command: str) -> None:
-    pass
+    with open(STORAGE_PATH, 'a+') as f:
+        f.write(command)
+        f.write("\n")
 
 
 def retrieve_all() -> List:
-    return ['test', 'test2', 'test3']
+    try:
+        with open(STORAGE_PATH, 'r') as f:
+            lines = [s.strip() for s in f.readlines()]
+
+        return lines
+    except Exception:
+        return []
+
+
+def flush() -> None:
+    os.remove(STORAGE_PATH)
+
+
+def get(num: int) -> None:
+    lines = retrieve_all()
+    return lines[num-1]
 
 
 @Request.application
@@ -22,9 +40,17 @@ def application(request):
 
     if req['instruction'] == 'register':
         store_command(req['body'])
+        res = 'A command was registered'
     
     if req['instruction'] == 'get_all':
         res = json.dumps(retrieve_all())
+
+    if req['instruction'] == 'get':
+        res = get(int(req['body']))
+
+    if req['instruction'] == 'flush':
+        flush()
+        res = 'Commands flushed'
 
     return Response(res)
 
