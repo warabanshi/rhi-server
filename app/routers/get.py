2d99643962d9dfc80df12be13e20511a592f80e2
warabanshi <warabanshi@gmail.com>
2021-09-29 22:55:34 +0900

    Fixed get instructions return format

--- app/routers/get.py
+++ app/routers/get.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import APIRouter
 
 from app.libraries.storage import retrieve_all
@@ -14,10 +16,12 @@ router = APIRouter(
 
 @router.get("/")
 async def get_all():
-    return {"result": retrieve_all(USER)}  # fixed user name is given temporary
+    r = retrieve_all(USER)  # fixed user name is given temporary
+    return {"result": r}
 
 
 @router.get("/{row_num}")
 async def get(row_num: int):
-    lines = retrieve_all(USER)
-    return {"result": f"{lines[row_num-1]}"}
+    lines = retrieve_all(USER)  # fixed user name is given temporary
+    r = lines[row_num - 1]
+    return {"result": r}
